23dc97dc5b06358ee7bf427b98366a4b00e4ceb6
mateuspaduan <mateusplc@hotmail.com>
2016-11-03 17:47:43 -0200

    Voltando ao normal

--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -7,8 +7,6 @@ public class Runner {
 	public static void main(String[] args) {
 		try{
 			LoginFrame window = new LoginFrame();
-			System.out.println("Teste");
-			System.out.println("Teste");
 			window.LoginFrame.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
