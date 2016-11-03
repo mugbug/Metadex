5fdd4e5f89be098ee833dd9c7ec48bf961b6feda
mateuspaduan <mateusplc@hotmail.com>
2016-11-03 17:45:06 -0200

    Teste

--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -7,6 +7,7 @@ public class Runner {
 	public static void main(String[] args) {
 		try{
 			LoginFrame window = new LoginFrame();
+			System.out.println("Teste");
 			window.LoginFrame.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
